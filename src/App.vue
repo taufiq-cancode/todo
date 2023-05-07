ad70ea7448fdd90cb0fec20b539b47f77dfa6774
taufiq-cancode <tee2dkay@gmail.com>
2023-05-07 08:01:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,700;1,400&display=swap");
+* {
+  font-family: "Cabin", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
